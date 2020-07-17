14f9ea19589f15ae616ebd72b03b9def04cca7d7
Gurkan Kaya <aligrknkya61@gmail.com>
2020-07-17 17:57:36 +0300

    Initial Commit

---
+++ Demo.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+
+using Demo.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
